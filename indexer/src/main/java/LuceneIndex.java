e05c2fed91d81ae6e44be3fa9c52295724a36f03
parthmangrola <parthmangrola@gmail.com>
2020-05-22 13:10:23 -0700

    init part 2

---
+++ indexer/src/main/java/LuceneIndex.java
@@ -0,0 +1,31 @@
+
+import jdk.nashorn.internal.parser.JSONParser;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.Field;
+import org.apache.lucene.index.CorruptIndexException;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.*;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class LuceneIndex {
+    public static void main (String[] args){
+        System.out.println("1");
+
+    }
+}
